4840224627c8d99c64d9b65b256b3365407362c0
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-02-22 02:26:13 -0500

    add

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,17 +1,42 @@
 cnf = data_bag_item("sensu", "config")
 master_address = cnf["master_address"] || node["ipaddress"]
-node.override["sensu"]["roles"] = (cnf[node.name] && cnf[node.name]["roles"]) || []
+
+if cnf[node.name] && cnf[node.name]["roles"]
+  node.override["eol_sensu_wrapper"]["roles"] = cnf[node.name]["roles"]
+end
+
 node.override["sensu"]["rabbitmq"]["host"] = master_address
 node.override["sensu"]["redis"]["host"] = master_address
 node.override["sensu"]["api"]["host"] = master_address
+
+if cnf["uchiva_user"]
+  node.override["uchiwa"]["settings"]["user"] = cnf["uchiva_user"]
+end
+
+if cnf["uchiva_password"]
+  node.override["uchiwa"]["settings"]["password"] =
+    cnf["uchiva_password"]
+end
+
+if cnf["uchiva_port"]
+  node.override["uchiwa"]["settings"]["port"] = cnf["uchiva_port"]
+end
+
+sensu_gem "sensu-plugin" do
+  version node["eol_sensu_wrapper"]["sensu_plugin_version"]
+end
+if node["eol_sensu_wrapper"]["roles"].include?("mysql")
+  sensu_gem "mysql"
+  sensu_gem "inifile"
+end
 
 include_recipe "sensu::default"
 
 if node["ipaddress"] == master_address
   include_recipe "sensu::rabbitmq"
   include_recipe "sensu::redis"
-  
-  sensu_checks = data_bag("sensu_checks").map do |item| 
+
+  sensu_checks = data_bag("sensu_checks").map do |item|
     data_bag_item("sensu_checks", item)
   end
 
@@ -33,16 +58,12 @@ end
 
 sensu_client node.name do
   address node["ipaddress"]
-  subscriptions node["sensu"]["roles"] + ["all"]
+  subscriptions node["eol_sensu_wrapper"]["roles"] + ["all"]
 end
 
-%w[check-disk.rb
-   check-load.rb
-   check-mem-pcnt.sh
-   mysql-replication-status.rb
-].each do |default_plugin|
-  cookbook_file "/etc/sensu/plugins/#{default_plugin}" do
-    source "plugins/#{default_plugin}"
+node["eol_sensu_wrapper"]["plugins"].each do |plugin|
+  cookbook_file "/etc/sensu/plugins/#{plugin}" do
+    source "plugins/#{plugin}"
     mode 0755
   end
 end
