024494c7ad09f08e71c13b50d41af4d8d05af8f1
Dmitry Mozzherin <dmozzherin@gmail.com>
2015-04-12 17:57:04 -0400

    remove plugins attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,12 +4,3 @@ override['uchiwa']['version'] = '0.4.0-1'
 
 default["eol_sensu_wrapper"]["roles"] = []
 default["eol_sensu_wrapper"]["sensu_plugin_version"] = "1.1.0"
-default["eol_sensu_wrapper"]["plugins"] = %w(
-  check-disk.rb
-  check-http.rb
-  check-load.rb
-  check-mem-pcnt.sh
-  check-mysql-alive.rb
-  check-mysql-replication.rb
-  metrics-system.rb
-)
